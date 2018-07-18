a2b9be32a581a7cf9c7049980f60e3acfe90974c
Megan Shaw <mmeganshaw@gmail.com>
2018-07-17 22:29:10 -0400

    adding most recent

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">GYFTY</h1>
+                <p className="lead">
+                  A place for busy people to exchange gift ideas.
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
--- validation/post.js
+++ validation/post.js
@@ -4,16 +4,20 @@ const isEmpty = require("./is-empty");
 module.exports = function validatePostInput(data) {
   let errors = {};
 
+  // running the same tests for our other fields
   data.text = !isEmpty(data.text) ? data.text : "";
 
-  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
-    errors.text = "Post must be between 10 and 300 characters";
+   // Checking that the text entered is between 6 and 30 characters
+   if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = "Password must be between 10 and 300 characters";
   }
 
+  // Running the isEmpty test for the text field
   if (Validator.isEmpty(data.text)) {
     errors.text = "Text field is required";
   }
-
+ 
+  // return object with the errors should there be any
   return {
     errors,
     isValid: isEmpty(errors)
